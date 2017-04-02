f8351dcabe813d7d5e83d56d39fd2b6a8d8b4ac5
U-NL\mgolovatyi <mgolovatyi@amam1l461.nl.flowtraders.local>
2017-04-02 21:09:41 +0200

    Add combinations module.

---
+++ combinations.scm
@@ -0,0 +1,19 @@
+
+
+(define (combinations-rep s)
+  "Return a list of combinations with repetitions"
+
+  (define (c1 e ll)
+    (map (lambda (l) (cons e l)) ll))
+
+  (define (c2 acc)
+    (apply append (map (lambda (e) (c1 e acc)) s)))
+
+  (define (c3 i acc)
+    (if (eq? i 0) acc (c3 (- i 1) (c2 acc))))
+
+  (c3 (lenght s) (list (list))))
+
+(eq?
+ (length (combinations-rep '(1 2 3)))
+ (expt 3 3))
